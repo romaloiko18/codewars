bee436c46407790cf86d59c1e3b97d31f4ecdbeb
romanloiko <roman.loiko@perfsol.tech>
2023-07-25 19:07:33 +0300

    Calculating with functions

---
+++ calculatingWithFunctions.js
@@ -0,0 +1,57 @@
+//Calculating with Functions
+//https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/javascript
+function zero(func) {
+    return func ? func(0) : 0
+}
+
+function one(func) {
+    return func ? func(1) : 1
+}
+
+function two(func) {
+    return func ? func(2) : 2
+}
+
+function three(func) {
+    return func ? func(3) : 3
+}
+
+function four(func) {
+    return func ? func(4) : 4
+}
+
+function five(func) {
+    return func ? func(5) : 5
+}
+
+function six(f) {
+    return func ? func(6) : 6
+}
+
+function seven(func) {
+    return func ? func(7) : 7
+}
+
+function eight(func) {
+    return func ? func(8) : 8
+}
+
+function nine(func) {
+    return func ? func(9) : 9
+}
+
+function plus(b) {
+    return (a) => a + b
+}
+
+function minus(b) {
+    return (a) => a - b
+}
+
+function times(b) {
+    return (a) => a * b
+}
+
+function dividedBy(b) {
+    return (a) => Math.floor(a / b)
+}
